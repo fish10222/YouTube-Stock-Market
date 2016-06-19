3b8aa440e01ebbdd7939262c71f6834b4f8cbc4b
Rodney Wang <rodneyw@sfu.ca>
2016-06-18 17:43:31 -0700

    seems to be able to login now

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :sessions
-  root 'users#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
