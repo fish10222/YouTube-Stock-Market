d4379f342bf4cbc9670b914cc5955da9e4a0b421
Rodney Wang <rodneyw@sfu.ca>
2016-06-18 18:34:12 -0700

    a header to help navigating

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,7 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+
   # Returns the current logged-in user (if any).
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -13,6 +14,11 @@ module SessionsHelper
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 
 end
