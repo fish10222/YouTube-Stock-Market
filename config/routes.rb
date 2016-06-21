754f2e8bec73ce9eab0ad01ce130f928bd9de89b
Henry Meng <fish10444@hotmail.com>
2016-06-20 19:16:30 -0700

    home, login, and signup button to not use hyperlink

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'sessions#new'
   get    'signup'  => 'users#new'
+  get    'home'    => 'sessions#new'
   get    'login'   => 'sessions#login'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
