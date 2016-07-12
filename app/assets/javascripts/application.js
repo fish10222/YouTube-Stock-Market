d7ce73d31b9b21b9e6a7f882baafc9330c934dc8
Henry Meng <fish10444@hotmail.com>
2016-07-11 20:21:44 -0700

    added profile tabs and bootstrap.js profile picture is just place holder for now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,13 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-material-design
+//= require bootstrap
+
+
+$(document).ready(function() {
+$(".btn-pref .btn").click(function () {
+    $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+    // $(".tab").addClass("active"); // instead of this do the below 
+    $(this).removeClass("btn-default").addClass("btn-primary");   
+});
+});
