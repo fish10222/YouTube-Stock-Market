41a1557df1aa596b0199c0bb89687a4983460633
Jayden McPhedrain <jaydenmcphedrain@hotmail.com>
2016-07-27 16:59:24 -0700

    don't know why it's crashing when trying to give the video a title

--- app/models/video_observer.rb
+++ app/models/video_observer.rb
@@ -1,9 +1,9 @@
 class VideoObserver < ActiveRecord::Observer
-  
+
   def before_save(resource)
     video = Yt::Video.new url: resource.link
     resource.uid = video.id
-    resource.title = video.title
+    resource.title = ""
     resource.likes = video.like_count
     resource.dislikes = video.dislike_count
     resource.published_at = video.published_at
