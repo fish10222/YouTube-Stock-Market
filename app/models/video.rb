e9664193d4446c0ec155efa26ce003ae72eb5df2
Jayden McPhedrain <jaydenmcphedrain@hotmail.com>
2016-07-27 17:18:50 -0700

    try that format instead?

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ActiveRecord::Base
   belongs_to :user
-  YT_LINK_FORMAT = /A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
+  YT_LINK_FORMAT = /A.*(youtube.com\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
   validates :link, presence: true, format: YT_LINK_FORMAT
 
 end
