a146fa9dc7a9335d7fb458feeeda7247920ed214
Jayden McPhedrain <jaydenmcphedrain@hotmail.com>
2016-07-28 22:41:34 -0700

    trying to set the price of the video to something reasonable - testing

--- app/models/video_observer.rb
+++ app/models/video_observer.rb
@@ -3,11 +3,11 @@ class VideoObserver < ActiveRecord::Observer
   def before_save(resource)
     video = Yt::Video.new url: resource.link
     resource.uid = video.id
-    resource.title = video.title
+    resource.title = video.views
     resource.likes = video.like_count
     resource.dislikes = video.dislike_count
     resource.published_at = video.published_at
-    resource.price = video.views since: 2.day.ago, until: 1.day.ago
+    #resource.price = video.views #since: 2.day.ago, until: 1.day.ago
   rescue Yt::Errors::NoItems
     resource.title = ''
   end
