762a44ed432198a2d937ee9f89516a3b6f5faa4a
Jayden McPhedrain <jaydenmcphedrain@hotmail.com>
2016-07-29 21:35:10 -0700

    adding some testing

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,6 @@ class Video < ActiveRecord::Base
   belongs_to :user
   YT_LINK_FORMAT = /\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
   validates :link, presence: true, format: YT_LINK_FORMAT
-  validates :points, greater_than_or_equal_to: 0
+  validates :points, greater_than_or_equal_to: {0}
 
 end
