1d4e7d20c84d1d5831e8e8f1c719d194e05a0743
Henry Meng <fish10444@hotmail.com>
2016-07-30 17:48:58 -0700

    fixing

--- Gemfile
+++ Gemfile
@@ -11,7 +11,6 @@ gem 'yourub'
 gem 'rails-observers'
 # google login gem
 gem "omniauth-google-oauth2", "~> 0.2.1"
-
 # puma
 gem 'puma'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/models/video.rb
+++ app/models/video.rb
@@ -2,5 +2,5 @@ class Video < ActiveRecord::Base
   belongs_to :user
   YT_LINK_FORMAT = /\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
   validates :link, presence: true, format: YT_LINK_FORMAT
-  validates :link, uniqueness: true
+
 end
