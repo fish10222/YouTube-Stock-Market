85dcc36889fbb5b17c137c31639e42ca3eb5d7a7
Jayden McPhedrain <jaydenmcphedrain@hotmail.com>
2016-07-30 19:13:16 -0700

    removed the purchasing stuff :(

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ActiveRecord::Base
   belongs_to :user
-  has_many :buyers, :class_name => 'User', :foreign_key => 'user_id'
+  #has_many :buyers, :class_name => 'User', :foreign_key => 'user_id'
   YT_LINK_FORMAT = /\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
   validates :link, presence: true, format: YT_LINK_FORMAT
   validates :link, uniqueness: true
