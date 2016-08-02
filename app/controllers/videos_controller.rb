ec104f828dc33e1c2c083e1f0919f437c7ed12a6
Henry Meng <fish10444@hotmail.com>
2016-08-02 03:02:50 -0700

    made the most basic of the purchase function

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,7 @@
 class VideosController < ApplicationController
   include SessionsHelper
   include ApplicationHelper
-  include VideosHelper
+
   def index
     @videos = Video.all.order('created_at DESC')
   end
@@ -13,24 +13,42 @@ class VideosController < ApplicationController
   def create
   @user = current_user
   @video = Video.new(video_params)
-  @video.user_id = @user.id
+  #@video.user_id = @user.id
     if @video.save
       flash[:success] = 'Video added!'
       redirect_to videos_path
     else
+      flash[:failure] = 'You don\'t have enough capital to purchase the video.'
       render :new
     end
   end
 
   def destroy
-    Video.find(params[:id]).destroy
+    @video = Video.find(params[:id])
+    @user = current_user
+    @user.update_attribute(:capital, @user.capital + @video.price) 
+    #Video.find(params[:id]).destroy
+    @video.destroy
     flash[:success] = "Video deleted"
-    redirect_to videos_path
+    redirect_to @user
   end
 
   def update
-
+    @video = Video.find(params[:id])
+    @user = current_user
+    if @user.capital > @video.price
+      @user.update_attribute(:capital, @user.capital- @video.price) 
+      @video.update_attribute(:user_id, @user.id)
+      #@video.buyers.create(@user)
+      #@user.purchases.create(@video.uid)
+      flash[:success] = 'Video Purchased!'
+      redirect_to @user
+    else
+      flash[:failure] = 'You don\'t have enough capital to purchase the video.'
+      redirect_to @user
+    end
   end
+
 
   private
     def video_params
--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,16 +1,5 @@
 module VideosHelper
 
-  def purchase
-    #@video = Video.find(params[:id])
-    #@user = current_user
-    if @user.capital > @video.price
-      @user.capital = @user.capital - @video.price
-      @video.buyers.create(@user)
-      @user.purchases.create(@video.uid)
-      flash[:success] = 'Video Purchased!'
-    else
-      flash[:failure] = 'You don\'t have enough capital to purchase the video.'
-    end
-  end
+  
 
 end
