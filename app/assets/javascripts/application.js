e7bbd65435b13e0853c8dee6fc2fe5982b0742df
Henry Meng <fish10444@hotmail.com>
2016-08-01 20:29:28 -0700

    fix for js not functioning properly

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,12 +19,6 @@
 
 
 //this is for Profile tab color switch/
-$(document).ready(function() {
-	$(".btn-pref .btn").click(function () {
-	    $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
-	    // $(".tab").addClass("active"); // instead of this do the below 
-	    $(this).removeClass("btn-default").addClass("btn-primary");   
-	});
-});
+
 
 
